25b3aed86c787a07671bdaec244c8f0202b84e64
Daniel Blezek <daniel.blezek@gmail.com>
2016-03-18 17:02:23 -0500

    Add create empty unsigned int texture

--- assets/js/create_difference_texture.js
+++ assets/js/create_difference_texture.js
@@ -27,3 +27,26 @@ function create_float_texture ( r, w, h ) {
   return differenceTexture;
 }
 
+// Create a texture buffer.
+//
+// args:
+//   r  -- registration object
+//   w  -- width of buffer
+//   h  -- height of buffer
+
+function create_texture ( r, w, h ) {
+  var gl = r.gl;
+  
+  var differenceTexture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, differenceTexture);
+  
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, w, h, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+  gl.bindTexture(gl.TEXTURE_2D, null);
+  return differenceTexture;
+}
+
