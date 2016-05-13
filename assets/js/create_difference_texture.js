8b57b7d4dd9fd5e5554218aa895d15c537a0a5a5
Daniel Blezek <daniel.blezek@gmail.com>
2016-05-13 13:55:30 -0500

    Use LINEAR in floating point textures

--- assets/js/create_difference_texture.js
+++ assets/js/create_difference_texture.js
@@ -18,8 +18,8 @@ function create_float_texture ( r, w, h ) {
   gl.bindTexture(gl.TEXTURE_2D, differenceTexture);
   
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, w, h, 0, gl.RGBA, gl.FLOAT, null);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
